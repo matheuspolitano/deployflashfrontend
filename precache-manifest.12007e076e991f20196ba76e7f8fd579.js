e95918df7750389da3ef04deed486f9008c0adc3
matheus.politano <matheuspolitano1@gmail.com>
2020-06-15 11:00:05 -0300

    First Commit

---
+++ precache-manifest.12007e076e991f20196ba76e7f8fd579.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "281e104236d9c161e5b1",
+    "url": "/css/app.7da1d2aa.css"
+  },
+  {
+    "revision": "b94da8ffcdffc96c4c5cc01ae610a61e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "281e104236d9c161e5b1",
+    "url": "/js/app.71c4e768.js"
+  },
+  {
+    "revision": "210b00ad8fdb0bfb5fe5",
+    "url": "/js/chunk-vendors.dd010599.js"
+  },
+  {
+    "revision": "af3442210705525bcba5cbbc7e4307bf",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
